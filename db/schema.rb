cbb59bb0070cf710360f5c86331c48fbef899fc0
Ryan Rempel <rgrempel@gmail.com>
2009-04-20 06:22:46 -0500

    Fix out-of-sync db/schema.rb.

--- db/schema.rb
+++ db/schema.rb
@@ -21,4 +21,25 @@ ActiveRecord::Schema.define(:version => 20090419204244) do
   add_index "scholar_sessions", ["session_id"], :name => "index_scholar_sessions_on_session_id"
   add_index "scholar_sessions", ["updated_at"], :name => "index_scholar_sessions_on_updated_at"
 
+  create_table "scholars", :force => true do |t|
+    t.string   "email",                             :null => false
+    t.string   "institution",                       :null => false
+    t.string   "full_name",                         :null => false
+    t.string   "crypted_password",                  :null => false
+    t.string   "password_salt",                     :null => false
+    t.string   "persistence_token",                 :null => false
+    t.string   "perishable_token",                  :null => false
+    t.integer  "login_count",        :default => 0, :null => false
+    t.integer  "failed_login_count", :default => 0, :null => false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "scholars", ["email"], :name => "index_scholars_on_email", :unique => true
+
 end
