bb0c3b76b8e28dcc9e7a7146737867e53b988aca
Ryan Rempel <rgrempel@gmail.com>
2009-04-20 06:42:14 -0500

    Start annotating models.

--- app/models/scholar.rb
+++ app/models/scholar.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: scholars
+#
+#  id                 :integer         not null, primary key
+#  email              :string(255)     not null
+#  institution        :string(255)     not null
+#  full_name          :string(255)     not null
+#  crypted_password   :string(255)     not null
+#  password_salt      :string(255)     not null
+#  persistence_token  :string(255)     not null
+#  perishable_token   :string(255)     not null
+#  login_count        :integer         default(0), not null
+#  failed_login_count :integer         default(0), not null
+#  last_request_at    :datetime
+#  current_login_at   :datetime
+#  last_login_at      :datetime
+#  current_login_ip   :string(255)
+#  last_login_ip      :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#
+
 class Scholar < ActiveRecord::Base
   acts_as_authentic do |c|
     c.perishable_token_valid_for 1.day
