d6f6574ce2ca4d2a76ff30f034846df6edb7e146
Ryan Rempel <rgrempel@gmail.com>
2009-04-21 22:55:43 -0500

    Tests! Woot!

--- test/unit/scholar_session_test.rb
+++ test/unit/scholar_session_test.rb
@@ -1,8 +1,37 @@
-require 'test_helper'
+require File.join(File.dirname(__FILE__), '..', 'test_helper')
 
 class ScholarSessionTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  setup :activate_authlogic
+
+  context "Given a scholar who is not activated, " do
+    setup do
+      @scholar = Scholar.new({
+        :email => "rgrempel@gmail.com", 
+        :full_name => "Ryan Rempel",
+        :institution => "CMU"
+      });
+      @scholar.password = "12345"
+      @scholar.password_confirmation = "12345"
+      @scholar.reset_perishable_token
+      @scholar.save
+    end
+
+    should "not be able to log in with password" do
+      assert_equal 1, Scholar.count, "Should have created a scholar"
+      
+      session = ScholarSession.new :email => "rgrempel@gmail.com", :password => "12345"  
+      assert !session.save, "Should have failed saving session"
+
+      @scholar.reload
+      assert_nil @scholar.activated_at, "Should not have marked activated_at"
+    end
+
+    should "be able to log in with password once activated" do
+      @scholar.activated_at = Time.now
+      @scholar.save
+ 
+      session = ScholarSession.new :email => "rgrempel@gmail.com", :password => "12345"  
+      assert session.save, "Should be able to log in with password now"
+    end
   end
 end
