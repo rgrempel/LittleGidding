7b76cc5942c60d3ae636d32790c2b87f64033c81
Ryan Rempel <rgrempel@gmail.com>
2009-05-12 18:22:28 -0500

    Just enough to get a blank SmartClient page.

--- app/controllers/scholar_sessions_controller.rb
+++ app/controllers/scholar_sessions_controller.rb
@@ -1,2 +1,5 @@
 class ScholarSessionsController < ApplicationController
+  def new
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :scholar_sessions
-
   map.resources :scholars
+
+  map.root :controller => "scholar_sessions", :action => "new"
 
   # The priority is based upon order of creation: first created -> highest priority.
 
@@ -42,6 +43,6 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  # map.connect ':controller/:action/:id'
+  # map.connect ':controller/:action/:id.:format'
 end
